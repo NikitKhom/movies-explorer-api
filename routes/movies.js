1127ea4c9da05157a15636fa2c1a598f95f67b8e
Nikita Khomenko <nikita.khomenko.n25@gmail.com>
2023-09-10 19:20:48 +0300

    fixes/ изменить параметры для модели movies

--- routes/movies.js
+++ routes/movies.js
@@ -11,15 +11,15 @@ movieRouter.get('/', getMovies);
 
 movieRouter.post('/', celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required().min(2).max(30),
-    director: Joi.string().required().min(2).max(30),
+    country: Joi.string().required().min(2).max(100),
+    director: Joi.string().required().min(2).max(100),
     duration: Joi.number().integer().required(),
     year: Joi.number().integer().min(1895).max(currentYear),
-    description: Joi.string().required().min(2).max(1000),
+    description: Joi.string().required().min(2).max(1500),
     image: Joi.string().required().pattern(REGEXP_LINK),
     trailer: Joi.string().required().pattern(REGEXP_LINK),
-    nameRU: Joi.string().required().min(2).max(30),
-    nameEN: Joi.string().required().min(2).max(30),
+    nameRU: Joi.string().required().min(2).max(100),
+    nameEN: Joi.string().required().min(2).max(100),
     thumbnail: Joi.string().required().pattern(REGEXP_LINK),
     movieId: Joi.number().integer().required(),
   }),
